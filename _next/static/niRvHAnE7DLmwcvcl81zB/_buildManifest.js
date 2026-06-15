e3546188c29dfa35ce932c5702b729b2dfe7f5d8
Joey Ara Teh <tjoeyara@gmail.com>
2026-06-16 00:48:49 +0800

    Updates

---
+++ _next/static/niRvHAnE7DLmwcvcl81zB/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/hey.Ara//_next/:path+",
+        "destination": "/hey.Ara/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
